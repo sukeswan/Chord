ed744c09917b6723861ac3c4940eeb4a4bb59f97
sukeswan <suryakeswani@gmail.com>
2021-11-28 22:28:21 -0800

    fixed classes to point to nodes

--- node.cpp
+++ node.cpp
@@ -2,16 +2,20 @@
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <cmath> 
 using namespace std;
-#define FINGERTABLESIZE 9 // 8 values but indexing starts at 1
-                          // finger tables will 0-8 but 0 will be left empty 
+#define MBITS 8
+#define INDEXOFFSET 1                           // 8 values but indexing starts at 1
+#define FINGERTABLESIZE (MBITS + INDEXOFFSET)   // finger tables will 0-8 but 0 will be left empty 
+
+class Node; // so compiler doesn't shit itself  
 
 class FingerTable{
     public: 
         int nodeId; // ID of node hosting finger table
-        int start[FINGERTABLESIZE]; // start interval values
         int intervalEnd[FINGERTABLESIZE]; // end interval values
         int succ[FINGERTABLESIZE];  // sucessor for start nodes
+        Node *nodes[FINGERTABLESIZE]; // for storing pointers to nodes
 
     FingerTable(){
         nodeId = -1; // default constructor for Node class
@@ -20,13 +24,9 @@ class FingerTable{
     FingerTable(int aNodeID){ // constructor with node ID 
         nodeId = aNodeID; 
     }
-      
-    void print(){ // print all values in the finger table printing: row num, interval, sucessor 
-        cout << "FingerTables:" << endl;
-        for(int i = 1; i < FINGERTABLESIZE; i++){
-            cout << "| k = " << i << " [" << start[i] << ", " << intervalEnd[i] << ")   " << "succ. = " << succ[i] << " |" << endl;
-        }
-    }
+
+    void print(); // print function for fingertable
+    void add(int rowNum, Node* newNode); // to add a value to the finger table 
 };
 
 class Node{
@@ -36,7 +36,7 @@ class Node{
         Node* sucessor; // pointer to sucessor node
         Node* predecessor; // pointer to predecessor node
         map<int, int> key_vals; // map to store the keys and values at this node
-    
+
     Node(){
 
     }
@@ -51,14 +51,13 @@ class Node{
     }
 
     void join(){ // this join is for the initial chord node joining the network 
-        for(int i = 1; i < FINGERTABLESIZE; i++){
-            table.start[i] = nodeId; 
-            table.intervalEnd[i] = nodeId;
-            table.succ[i] = nodeId; 
-        }
+        
+        predecessor = this; // node point to itself 
+        sucessor = this; 
 
-        predecessor = new Node(nodeId); // allocate predecessor and successor in memeory 
-        sucessor = new Node(nodeId); 
+        for(int i = 1; i < FINGERTABLESIZE; i++){
+            table.add(i,this);
+        }
         printFT(); 
         return; 
     }
@@ -100,27 +99,43 @@ class Node{
 
 }; 
 
+void FingerTable::print(){
+    for(int i = 1; i < FINGERTABLESIZE; i++){
+        cout << "| k = " << i << " [" << nodes[i]->nodeId << ", " << intervalEnd[i] << ")   " << "succ. = " << succ[i] << " |" << endl;
+    }
+}
+
+void FingerTable::add(int rowNum, Node* newNode){ // table.add(1,&node)
+    nodes[rowNum] = newNode; 
+    succ[rowNum] = newNode->sucessor->nodeId; 
+    int dist = pow(2, rowNum-1);
+    int div = pow(2, MBITS);
+    intervalEnd[rowNum] = (newNode->nodeId + dist) % div; // calculate end interval for node
+}
+
 void test_fingerTablePrint(){ // test creating FingerTable and printing it 
     
-    FingerTable test(8); // initialized with node ID 8 
-
-    for(int i = 1; i < FINGERTABLESIZE; i++){
-        test.start[i] = i; 
-        test.intervalEnd[i] = i+1; 
-        test.succ[i] = i+2; 
-    }
+    Node one(1); 
+    Node two(2);
+    one.sucessor = &two; 
     
-    test.print(); 
+    FingerTable test(0); 
+   
+    for(int i = 1; i < FINGERTABLESIZE; i++){
+        test.add(i,&one); 
+    }
+
+    test.print();
 }
 
 void test_nodePrints(){ // test both node print functions
     
     Node testNode(8); 
+    Node check(500); 
+    Node next(501);
+    check.sucessor = &next; 
     for(int i = 1; i < FINGERTABLESIZE; i++){
-        testNode.table.start[i] = i; 
-        testNode.table.intervalEnd[i] = i; 
-        testNode.table.succ[i] = i; 
-
+        testNode.table.add(i,&check); 
         testNode.key_vals.insert(pair<int, int>(i, i*i));
 
     } 
@@ -130,9 +145,11 @@ void test_nodePrints(){ // test both node print functions
 
 }
 
-int main(){
+void test_nodeJoin(){ // test first node joining the chord network 
     Node only(3); 
-
     only.join();
+}
+
+int main(){ 
     return 0;
 }
